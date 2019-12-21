cce57caaf95a74296da70952c5cd281d9329df6e
Omondi14 <eomondi@college.harvard.edu>
2019-12-21 23:56:19 +0300

    Changed server code to use postgres and setting up user table

--- server/app.js
+++ server/app.js
@@ -4,42 +4,43 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+const { Pool } = require('pg')
 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'Ernest1418!',
+  post: 7000
+})
+
+module.exports = pool;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testRoute');
 
-var app = express();
+var router = express.Router();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+router.get('/server/get/userprofilefromdb', (req, res, next) => {
+  const email = req.query.email
+  console.log(email)
+  pool.query(`SELECT * FROM users
+              WHERE email=$1`, [ email ],
+              (q_err, q_res) => {
+                res.json(q_res.rows)
+      })
+} )
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testRoute", testAPIRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Retrieve another users profile from db based on username
+router.get('/server/get/otheruserprofilefromdb', (req, res, next) => {
+  // const email = [ "%" + req.query.email + "%"]
+  const username = String(req.query.username)
+  pool.query(`SELECT * FROM users
+              WHERE username = $1`,
+    [ username ], (q_err, q_res) => {
+    res.json(q_res.rows)
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = router;
